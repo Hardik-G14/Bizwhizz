e80ab09989f43a0f19b0846c7008f397c4d8e123
Hardik <hardikgathibandhe14@gmail.com>
2021-10-06 17:13:03 +0530

    first commit

---
+++ js/slideranimation.js
@@ -0,0 +1,28 @@
+const leftArrow=document.querySelector('.left-arrow');
+const rightArrow=document.querySelector('.right-arrow');
+const slider=document.querySelector('.carousel__slider');
+
+var cardIndex;
+var slidenum;
+
+
+rightArrow.addEventListener('click', function() {
+	cardIndex=document.querySelector('input[name="slide"]:checked').value;
+	if(cardIndex<3){
+		cardIndex++;
+	}
+	else{
+		cardIndex=1;
+	}
+	slidenum='slide'+cardIndex;
+	document.getElementById(slidenum).checked=true;
+});
+leftArrow.addEventListener('click', function() {
+	cardIndex=document.querySelector('input[name="slide"]:checked').value;
+	cardIndex--;
+	if(cardIndex<1){
+		cardIndex=3;
+	}
+	slidenum='slide'+cardIndex;
+	document.getElementById(slidenum).checked=true;
+});
